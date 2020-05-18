ded1d5f5c89431aa75018a48bfc1043a78994de9
a.petrushin <a.petrushin@corp.mail.ru>
2020-05-18 15:55:19 +0300

    array

--- dist/code.js
+++ dist/code.js
@@ -1,28 +1,40 @@
 figma.showUI(__html__);
 figma.ui.onmessage = msg => {
     if (msg.type === 'create-style') {
-        let selection = figma.currentPage.selection;
-        for (let node of selection) {
-            if (node.type == "TEXT") {
-                createStyle(node);
-            }
-        }
+        parseSelection();
     }
     else if (msg.type === "alert") {
         figma.notify(msg.message);
     }
 };
 figma.on("selectionchange", () => {
-    let selection = figma.currentPage.selection;
-    for (let node of selection) {
-        if (node.type == "TEXT") {
-            createStyle(node);
-        }
-    }
+    parseSelection();
 });
-function createStyle(node) {
+function parseSelection() {
+    let selection = figma.currentPage.selection;
+    let parent = new FlutterParent();
+    for (let node of selection) {
+        parseNode(node, parent);
+    }
+    figma.ui.postMessage(parent.toString());
+}
+function parseNode(node, parent) {
+    console.log(node.type);
+    if (node.type == "TEXT") {
+        parent.addChild(textWidget(node));
+    }
+    else if (node.type == "GROUP" || node.type == "INSTANCE") {
+        let groupParent = new FlutterParent();
+        for (let child of node.children) {
+            console.log(child.type);
+            parseNode(child, groupParent);
+        }
+        parent.addChild(groupParent);
+    }
+}
+function textWidget(node) {
     let code = "";
-    let text = node.characters;
+    let text = "\"" + node.characters + "\"";
     let fontSize = node.fontSize;
     //TODO support fontFamily
     //var fontFamily = node.fontName["family"];
@@ -44,6 +56,30 @@ function createStyle(node) {
             textAlign = "TextAlign.justify";
             break;
     }
+    switch (node.textCase) {
+        case "LOWER":
+            text += ".toLowerCase()";
+            break;
+        case "TITLE":
+            text = "\"" + node.characters.charAt(0).toUpperCase() + node.characters.substring(1) + "\"";
+            break;
+        case "UPPER":
+            text += ".toUpperCase()";
+            break;
+    }
+    let decoration = null;
+    switch (node.textDecoration) {
+        //skip default
+        // case "NONE":
+        //     decoration = "TextDecoration.none";
+        //     break;
+        case "UNDERLINE":
+            decoration = "TextDecoration.underline";
+            break;
+        case "STRIKETHROUGH":
+            decoration = "TextDecoration.overline";
+            break;
+    }
     let fontWeight = null;
     switch (style) {
         // skip default
@@ -61,12 +97,13 @@ function createStyle(node) {
     textStyle.addArgument("color", color);
     textStyle.addArgument("fontSize", fontSize.toFixed(1));
     textStyle.addArgument("fontWeight", fontWeight);
+    textStyle.addArgument("decoration", decoration);
     let textWidget = new FlutterObject("Text");
-    textWidget.addValue("\"" + text + "\"");
+    textWidget.addValue(text);
     textWidget.addArgument("style", textStyle);
     textWidget.addArgument("textAlign", textAlign);
     code += textWidget;
-    figma.ui.postMessage(code);
+    return code;
 }
 class FlutterObject {
     constructor(name) {
@@ -86,6 +123,7 @@ class FlutterObject {
             this.params.push(name + ": " + value.toString());
         }
     }
+    // noinspection JSUnusedGlobalSymbols
     addArgumentOpt(name, value) {
         if (value == null) {
             this.params.push(name + ": null");
@@ -96,6 +134,26 @@ class FlutterObject {
     }
     toString() {
         return this.name + "(" + this.params.join(", ") + ")";
+    }
+}
+class FlutterParent {
+    constructor() {
+        this.name = "Group";
+        this.children = [];
+    }
+    addChild(value) {
+        this.children.push(value);
+    }
+    toString() {
+        if (this.children.length > 1) {
+            return this.name + "(\n" +
+                "  children: [" +
+                "\n   " + this.children.join(",\n") + "],\n" +
+                ");";
+        }
+        else {
+            return this.children.join("");
+        }
     }
 }
 class FlutterColor {
@@ -112,6 +170,7 @@ class FlutterColor {
             70: "Colors.white70",
             100: "Colors.white"
         };
+        this.whiteHexEnd = "FFFFFF";
         this.black = {
             12: "Colors.black12",
             26: "Colors.black26",
@@ -121,14 +180,17 @@ class FlutterColor {
             87: "Colors.black87",
             100: "Colors.black"
         };
+        this.blackHexEnd = "000000";
         this.not_parsed = "Colors.black";
     }
     fromTextNode(node) {
-        return this.fromPaint(node.fills[0]);
+        let fill = node.fills[0];
+        return this.fromPaint(fill, node.opacity);
     }
-    fromPaint(fill) {
+    fromPaint(fill, opacity) {
         if (fill.type === "SOLID") {
-            return this.color(fill.color, fill.opacity);
+            let roundOpacity = Math.round(fill.opacity * opacity * 100.0) / 100;
+            return this.color(fill.color, roundOpacity);
         }
         else {
             return this.not_parsed;
@@ -137,13 +199,13 @@ class FlutterColor {
     color(rgb, o) {
         let opacity = Math.round(o * 100);
         let hexColor = "" + hex(o) + hex(rgb.r) + hex(rgb.g) + hex(rgb.b);
-        if (hexColor.endsWith("FFFFFF")) {
+        if (hexColor.endsWith(this.whiteHexEnd)) {
             let white = this.white[opacity];
             if (white != null) {
                 return white;
             }
         }
-        if (hexColor.endsWith("000000")) {
+        if (hexColor.endsWith(this.blackHexEnd)) {
             let black = this.black[opacity];
             if (black != null) {
                 return black;
--- src/main/code.ts
+++ src/main/code.ts
@@ -1,175 +1,243 @@
 figma.showUI(__html__);
 
 figma.ui.onmessage = msg => {
-  if (msg.type === 'create-style') {
-    let selection = figma.currentPage.selection;
-    for (let node of selection) {
-      if (node.type == "TEXT") {
-        createStyle(node)
-      }
+    if (msg.type === 'create-style') {
+        parseSelection();
+    } else if (msg.type === "alert") {
+        figma.notify(msg.message)
     }
-  } else if (msg.type === "alert") {
-    figma.notify(msg.message)
-  }
 };
 
 figma.on("selectionchange", () => {
-    let selection = figma.currentPage.selection;
-    for (let node of selection) {
-      if (node.type == "TEXT") {
-        createStyle(node)
-      }
-    }
+    parseSelection()
 });
 
-function createStyle(node: TextNode) {
-  let code = "";
-  let text = node.characters;
-  let fontSize = node.fontSize as number;
-  //TODO support fontFamily
-  //var fontFamily = node.fontName["family"];
-  let style = (node.fontName as FontName)["style"];
-  let color = new FlutterColor().fromTextNode(node);
-  let textAlign = null;
-  switch (node.textAlignHorizontal) {
-  	//skip default
-    // case "LEFT":
-    //   textAlign = "TextAlign.start"
-    //   break;
-    case "CENTER":
-      textAlign = "TextAlign.center";
-      break;
-    case "RIGHT":
-      textAlign = "TextAlign.end";
-      break;
-    case "JUSTIFIED":
-      textAlign = "TextAlign.justify";
-      break;
-  }
-  let fontWeight = null;
-  switch (style) {
-  	// skip default
-  	// case "Regular":
-	  // fontWeight = "FontWeight.normal";
-	  // break;
-    case "Semibold":
-      fontWeight = "FontWeight.w600";
-      break;
-    case "Bold":
-      fontWeight = "FontWeight.bold";
-      break
-  }
+function parseSelection() {
+    let selection = figma.currentPage.selection;
 
-  let textStyle = new FlutterObject("TextStyle");
-  textStyle.addArgument("color", color);
-  textStyle.addArgument("fontSize", fontSize.toFixed(1));
-  textStyle.addArgument("fontWeight", fontWeight);
+    let parent = new FlutterParent();
+    for (let node of selection) {
+        parseNode(node, parent)
+    }
+    figma.ui.postMessage(parent.toString())
+}
 
-  let textWidget = new FlutterObject("Text");
-  textWidget.addValue("\"" + text + "\"");
-  textWidget.addArgument("style", textStyle);
-  textWidget.addArgument("textAlign", textAlign);
+function parseNode(node: SceneNode, parent: FlutterParent) {
+    console.log(node.type);
+    if (node.type == "TEXT") {
+        parent.addChild(textWidget(node));
+    } else if (node.type == "GROUP" || node.type == "INSTANCE") {
+        let groupParent = new FlutterParent();
+        for (let child of node.children) {
+           console.log(child.type);
+           parseNode(child, groupParent)
+        }
+        parent.addChild(groupParent);
+    }
+}
 
-  code+= textWidget;
-  figma.ui.postMessage(code)
+function textWidget(node: TextNode): string {
+    let code = "";
+    let text = "\"" + node.characters + "\"";
+    let fontSize = node.fontSize as number;
+    //TODO support fontFamily
+    //var fontFamily = node.fontName["family"];
+    let style = (node.fontName as FontName)["style"];
+    let color = new FlutterColor().fromTextNode(node);
+    let textAlign = null;
+    switch (node.textAlignHorizontal) {
+        //skip default
+        // case "LEFT":
+        //   textAlign = "TextAlign.start"
+        //   break;
+        case "CENTER":
+            textAlign = "TextAlign.center";
+            break;
+        case "RIGHT":
+            textAlign = "TextAlign.end";
+            break;
+        case "JUSTIFIED":
+            textAlign = "TextAlign.justify";
+            break;
+    }
+    switch (node.textCase) {
+        case "LOWER":
+            text += ".toLowerCase()";
+            break;
+        case "TITLE":
+            text = "\"" + node.characters.charAt(0).toUpperCase() + node.characters.substring(1) + "\"";
+            break;
+        case "UPPER":
+            text += ".toUpperCase()";
+            break;
+    }
+    let decoration = null;
+    switch (node.textDecoration) {
+        //skip default
+        // case "NONE":
+        //     decoration = "TextDecoration.none";
+        //     break;
+        case "UNDERLINE":
+            decoration = "TextDecoration.underline";
+            break;
+        case "STRIKETHROUGH":
+            decoration = "TextDecoration.overline";
+            break;
+    }
+    let fontWeight = null;
+    switch (style) {
+        // skip default
+        // case "Regular":
+        // fontWeight = "FontWeight.normal";
+        // break;
+        case "Semibold":
+            fontWeight = "FontWeight.w600";
+            break;
+        case "Bold":
+            fontWeight = "FontWeight.bold";
+            break
+    }
+
+    let textStyle = new FlutterObject("TextStyle");
+    textStyle.addArgument("color", color);
+    textStyle.addArgument("fontSize", fontSize.toFixed(1));
+    textStyle.addArgument("fontWeight", fontWeight);
+    textStyle.addArgument("decoration", decoration);
+
+    let textWidget = new FlutterObject("Text");
+    textWidget.addValue(text);
+    textWidget.addArgument("style", textStyle);
+    textWidget.addArgument("textAlign", textAlign);
+
+    code += textWidget;
+    return code;
 }
 
 class FlutterObject {
-  params: any[] = [];
-  name: String;
-  constructor(name: String) {
-    this.name = name
-  }
+    params: any[] = [];
+    name: String;
 
-  addValue(value: any) {
-    if (value != null) {
-      this.params.push(value.toString())
-    } else {
-      this.params.push("null")
+    constructor(name: String) {
+        this.name = name
     }
-  }
-  addArgument(name: string, value: any) {
-    if (value != null) {
-      this.params.push(name + ": " + value.toString())
-    }
-  }
-  addArgumentOpt(name: string, value: string) {
-    if (value == null) {
-      this.params.push(name + ": null")
-    } else {
-      this.params.push(name + ": " + value.toString())
-    }
-  }
 
-  toString() {
-    return this.name + "(" + this.params.join(", ") + ")";
-  }
+    addValue(value: any) {
+        if (value != null) {
+            this.params.push(value.toString())
+        } else {
+            this.params.push("null")
+        }
+    }
+
+    addArgument(name: string, value: any) {
+        if (value != null) {
+            this.params.push(name + ": " + value.toString())
+        }
+    }
+
+    // noinspection JSUnusedGlobalSymbols
+    addArgumentOpt(name: string, value: string) {
+        if (value == null) {
+            this.params.push(name + ": null")
+        } else {
+            this.params.push(name + ": " + value.toString())
+        }
+    }
+
+    toString() {
+        return this.name + "(" + this.params.join(", ") + ")";
+    }
+}
+
+class FlutterParent {
+    name: string = "Group";
+    children: any[] = [];
+
+    addChild(value: any) {
+        this.children.push(value)
+    }
+
+    toString() {
+        if (this.children.length > 1) {
+            return this.name + "(\n" +
+                "  children: [" +
+                "\n   " + this.children.join(",\n") + "],\n" +
+                ");"
+        } else {
+            return this.children.join("");
+        }
+    }
 }
 
 class FlutterColor {
 
-  fromTextNode(node: TextNode): string {
-    return this.fromPaint((node.fills as ReadonlyArray<Paint>)[0])
-  }
-
-  fromPaint(fill: Paint): string {
-    if (fill.type === "SOLID") {
-      return this.color(fill.color, fill.opacity)
-    } else {
-      return this.not_parsed;
+    fromTextNode(node: TextNode): string {
+        let fill = (node.fills as ReadonlyArray<Paint>)[0];
+        return this.fromPaint(fill, node.opacity)
     }
-  }
 
-  color(rgb: RGB, o: number): string {
-    let opacity = Math.round(o * 100);
-    let hexColor = "" + hex(o) + hex(rgb.r) + hex(rgb.g) + hex(rgb.b);
-    if (hexColor.endsWith("FFFFFF")) {
-      let white = this.white[opacity];
-      if (white != null) {
-        return white
-      }
+    fromPaint(fill: Paint, opacity: number): string {
+        if (fill.type === "SOLID") {
+            let roundOpacity = Math.round(fill.opacity * opacity * 100.0) / 100;
+            return this.color(fill.color, roundOpacity)
+        } else {
+            return this.not_parsed;
+        }
     }
-    if (hexColor.endsWith("000000")) {
-      let black: string = this.black[opacity];
-      if (black != null) {
-        return black
-      }
+
+    color(rgb: RGB, o: number): string {
+        let opacity = Math.round(o * 100);
+        let hexColor = "" + hex(o) + hex(rgb.r) + hex(rgb.g) + hex(rgb.b);
+        if (hexColor.endsWith(this.whiteHexEnd)) {
+            let white = this.white[opacity];
+            if (white != null) {
+                return white
+            }
+        }
+        if (hexColor.endsWith(this.blackHexEnd)) {
+            let black: string = this.black[opacity];
+            if (black != null) {
+                return black
+            }
+        }
+        if (opacity === 0) {
+            return this.transparent
+        }
+        return "Color(0x" + hexColor + ")";
     }
-    if (opacity === 0) {
-      return this.transparent
-    }
-    return "Color(0x" + hexColor + ")";
-  }
 
-  transparent = "Colors.transparent";
+    transparent = "Colors.transparent";
 
-  white: {[index: number]:string} = {
-    10: "Colors.white10",
-    12: "Colors.white12",
-    24: "Colors.white24",
-    30: "Colors.white30",
-    38: "Colors.white38",
-    54: "Colors.white54",
-    60: "Colors.white60",
-    70: "Colors.white70",
-    100: "Colors.white"
-  };
+    white: { [index: number]: string } = {
+        10: "Colors.white10",
+        12: "Colors.white12",
+        24: "Colors.white24",
+        30: "Colors.white30",
+        38: "Colors.white38",
+        54: "Colors.white54",
+        60: "Colors.white60",
+        70: "Colors.white70",
+        100: "Colors.white"
+    };
 
-  black: {[index: number]:string} = {
-    12: "Colors.black12",
-    26: "Colors.black26",
-    38: "Colors.black38",
-    45: "Colors.black45",
-    54: "Colors.black54",
-    87: "Colors.black87",
-    100: "Colors.black"
-  };
+    whiteHexEnd = "FFFFFF";
 
-  not_parsed = "Colors.black"
+    black: { [index: number]: string } = {
+        12: "Colors.black12",
+        26: "Colors.black26",
+        38: "Colors.black38",
+        45: "Colors.black45",
+        54: "Colors.black54",
+        87: "Colors.black87",
+        100: "Colors.black"
+    };
 
+    blackHexEnd = "000000";
+
+    not_parsed = "Colors.black"
 }
 
 
 function hex(double: number): String {
-  return (Math.round(double * 255)).toString(16).toUpperCase().padStart(2, "0");
+    return (Math.round(double * 255)).toString(16).toUpperCase().padStart(2, "0");
 }
